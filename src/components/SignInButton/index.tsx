d335abd01bfd77ede2c4b0dab2ae12caa7e46283
Jorge Felipe <jfss1@ifal.edu.br>
2022-05-20 09:28:46 -0300

    add authentication

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,27 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { useSession, signIn, signOut } from "next-auth/react"
 
 import styles from './styles.module.scss'
 
+
 export function SignInButton() {
-  const isUserLoggedIn = true
-  return isUserLoggedIn ? (
+  const { data: session } = useSession();
+
+  return session ? (
     <button 
       type="button" 
       className={styles.signInButton}
     >
       <FaGithub color='#04d361' />
         Ramon Barros
-      <FiX color='#747480' className={styles.closeIcon} />
+      <FiX color='#747480' className={styles.closeIcon} onClick={( () => signOut())}/>
     </button>
   ) : (
     <button 
       type="button" 
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color='#eba417' />
       Sign in with Github
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import type { NextPage } from 'next'
+import { GetStaticProps } from 'next';
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-const Home: NextPage = () => {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: string;
+  }
+}
+
+const Home: NextPage<HomeProps> = ({ product }) => {
   return (
     <>
       <Head>
@@ -22,16 +31,35 @@ const Home: NextPage = () => {
 
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
         <img src="/images/avatar.svg" alt="Avatar" />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1L17liIUar9keVlIjJUfYutJ'); 
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format((price.unit_amount ?? 100) / 100)
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
+  }
 }
 
 export default Home
