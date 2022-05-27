a9ab861c757117d240c14ecc24ef3b0c1ce462fa
Jorge Felipe <jfss1@ifal.edu.br>
2022-05-26 21:18:25 -0300

    wip

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,44 @@
+import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { stripe } from '../../services/stripe';
+import { getStripeJS } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButton{
   priceId: string;
 }
 export function SubscribeButton({priceId}: SubscribeButton) {
-  console.log(priceId)
+  const { data: session } = useSession()
+
+  const handleSubmit = async () => {
+    if(!session){
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      console.log(response)
+
+      const { sessionId } = response.data;
+
+
+      const stripeJs = await getStripeJS()
+
+      console.log(stripeJs)
+
+      await stripeJs?.redirectToCheckout({ sessionId })
+
+    } catch( e ) {
+      alert(e)
+    }
+  }
   return (
     <button
       type="button"
       className={styles.subcribeButton}
+      onClick={handleSubmit}
     >
       Subscribe now
     </button>
